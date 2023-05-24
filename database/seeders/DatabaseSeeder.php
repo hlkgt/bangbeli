881fbffe9cef7ac80b013ae9409dd26613ad5d5a
hlkgt <haihaioell@gmail.com>
2023-05-24 13:57:46 +0700

    migration and seed product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategoriSeeder::class
+            CategoriSeeder::class,
+            ProductSeeder::class
         ]);
     }
 }
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products = [
+            ["categori_id"=>1,"name"=>"Nasi Goreng","description"=>"Nasi Goreng Lejat Begiji","price"=>13.000,"rate"=>4,"stock"=>10,'sold'=>0],
+            ["categori_id"=>2,"name"=>"Lemon Tea","description"=>"Nasi Goreng Lejat Begiji","price"=>13.000,"rate"=>4,"stock"=>10,'sold'=>0],
+            ["categori_id"=>3,"name"=>"Kentang Goyeng","description"=>"Nasi Goreng Lejat Begiji","price"=>13.000,"rate"=>4,"stock"=>10,'sold'=>0]
+        ];
+        $products = collect($products)->map(function ($product) {
+            return [
+                'categori_id' => $product['categori_id'],
+                'name' => $product['name'],
+                'description' => $product['description'],
+                'price' => $product['price'],
+                'rate' => $product['rate'],
+                'stock' => $product['stock'],
+                'sold' => $product['sold']
+            ];
+        });
+    
+        Product::insert($products->toArray());
+
+    }
+}
